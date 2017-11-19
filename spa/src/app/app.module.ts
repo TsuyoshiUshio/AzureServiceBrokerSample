de945158863cc02fd5b04fdd6564bc6845a3e5e6
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-11-20 00:49:55 +0900

    Adding Material Design and broker for AzureServiceBroker

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HtmlParser } from '@angular/compiler';
+import { BrowserAnimationBuilder } from '@angular/platform-browser/animations/src/animation_builder';
 
 
 @NgModule({
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
